a87531b6d5ed2e000547828c1d20ce3258d23320
LiuZhen <lzhen.dev@outlook.com>
2022-06-24 18:02:26 +0800

    Initial commit

---
+++ Uni_Appstore/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# def hello(request):
+#     return HttpResponse("Hello world ! ")
+
+
+def hello(request):
+    context = {
+        'Title': 'Hello World!',
+        'hello': 'hi'
+    }
+    return render(request, 'runoob.html', context)
